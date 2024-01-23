9446a1b4a2d62a534bc1efedb602f737ae79136a
Om Rawal <omrawal007@gmail.com>
2024-01-23 21:15:48 +0530

    added basic client server

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+HEADER = 64 
+PORT = 5050
+# SERVER = 'localhost'
+SERVER = socket.gethostbyname(socket.gethostname()) # To connect to any computer on internet change this to your public IP address
+ADDR = (SERVER,PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+# print(socket.gethostname())
+# print(SERVER)
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn,addr):
+    print(f"[NEW CONNECTION] {addr} connceted.")
+    connected = True
+
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}.")
+            conn.send("Msg received".encode(FORMAT))
+    conn.close()
+
+
+def start():
+    print(f"[STARTING] Server is starting...")
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn ,addr = server.accept()
+        thread = threading.Thread(target=handle_client,args=(conn,addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+start()
